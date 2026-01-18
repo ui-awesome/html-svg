efe56a8c3aa9cb52fef99ee09a7f96e56f9d8cd9
Wilmer Arambula <terabytesoftw@gmail.com>
2026-01-18 12:17:47 -0300

    Apply fixed Coderrabitai review.

--- tests/Support/Provider/Attribute/MarkerWidthProvider.php
+++ tests/Support/Provider/Attribute/MarkerWidthProvider.php
@@ -7,7 +7,7 @@ namespace UIAwesome\Html\Svg\Tests\Support\Provider\Attribute;
 /**
  * Data provider for {@see \UIAwesome\Html\Svg\Tests\Attribute\HasMarkerWidthTest}.
  *
- * Provides representative input/output pairs for testing the `d` attribute functionality.
+ * Provides representative input/output pairs for testing the `markerWidth` attribute functionality.
  *
  * @copyright Copyright (C) 2026 Terabytesoftw.
  * @license https://opensource.org/license/bsd-3-clause BSD 3-Clause License.
